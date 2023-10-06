f17b1e364ffa27a2976d67cd24a6e3761d2cd73f
Muhammad Awais Shafi <awaisshafi6164@gmail.com>
2023-10-06 20:27:40 +0500

    Initial commit

---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '/constants/colors.dart';
+import '/model/todo.dart';
+
+class ToDoItem extends StatelessWidget {
+  const ToDoItem({
+    super.key,
+    required this.todo,
+    required this.onToDoChanged,
+    required this.onDeleteItem,
+  });
+
+  final ToDo todo;
+  final onToDoChanged;
+  final onDeleteItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child: ListTile(
+        onTap: () {
+          //print('Cliked on To Do Item');
+          onToDoChanged(todo);
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+        tileColor: Colors.white,
+        leading: Icon(
+            todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+            color: tdBlue),
+        title: Text(
+          todo.todoText!,
+          style: TextStyle(
+              fontSize: 16,
+              color: tdBlack,
+              decoration: todo.isDone ? TextDecoration.lineThrough : null),
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+              color: tdRed, borderRadius: BorderRadius.circular(5)),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              //print('Cliked on delete icon');
+              onDeleteItem(todo.id);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
